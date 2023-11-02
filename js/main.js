378be92357b7e4c6dbc7d65bff948f9b4d866b0a
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-02 16:33:38 +0100

    added email list

--- js/main.js
+++ js/main.js
@@ -7,8 +7,24 @@ console.log(axios);
 createApp({
     data() {
         return {
-            message: 'Ciao Vue!'
+            email: [],
         }
+    },
+    methods: {
+        fetchEmail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        // console.log(res, res.data);
+                        const result = res.data.response;
+                        this.email.push(result);
+                        // console.log(this.email);
+                    })
+            }
+        }
+    },
+    created() {
+        this.fetchEmail();
     },
     mounted() {
         console.log('OK');
